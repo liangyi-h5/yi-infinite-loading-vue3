e71a26a846664a8c3c0ed885d25a8d92d730ee3f
李良贤 <liangxian.li@patpat.com>
2023-12-19 16:34:08 +0800

    fix: build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === 'production'
 export default defineConfig({
+  base: isProduction? '/yi-infinite-loading-vue3/' : '/',
   plugins: [vue()],
 })
